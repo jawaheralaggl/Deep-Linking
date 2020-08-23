aee36c10cd2fda5b1402292bc6a3d57af2adf9e1
jawaheralaggl <jawaheralaggl@gmail.com>
2020-08-24 01:14:59 +0300

    first commit

--- deepLinkFirst/ViewController.swift
+++ deepLinkFirst/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func goToSecondApp(_ sender: UIButton) {
+        
+        let application = UIApplication.shared
+        
+        let secondAppPath = "second://"
+        
+        let appUrl = URL(string: secondAppPath)!
+        
+        let websiteUrl = URL(string: "https://www.billeasy.in")!
+        
+        if application.canOpenURL(appUrl) {
+            
+            application.open(appUrl, options: [:], completionHandler: nil)
+            
+        }else{
+            
+            application.open(websiteUrl)
+        }
+        
+    }
+    
 }
 
